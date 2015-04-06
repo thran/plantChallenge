458d7508eefc695d0a7d329d2d8d94f43a89f374
Thran <exthran@gmail.com>
2015-04-07 00:11:49 +0200

    fix

--- practice/context_processors.py
+++ practice/context_processors.py
@@ -2,4 +2,4 @@ from django.conf import settings
 
 
 def important(request):
-    return {'GOOGLE_ANALYTICS': settings.ON_SERVER and not settings.DEVEL}
+    return {'GOOGLE_ANALYTICS': settings.ON_SERVER and not settings.DEBUG}
