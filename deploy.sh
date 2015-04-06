203b5ccb10a7c0c2c02ac7e1030e9a053c3053c1
Thran <exthran@gmail.com>
2015-04-06 10:11:32 +0200

    add loading data to deploy

--- deploy.sh
+++ deploy.sh
@@ -13,5 +13,9 @@ pip install -r $DIR/requirements.txt
 python $DIR/manage.py syncdb
 python $DIR/manage.py migrate
 
+python $DIR/manage.py load_flashcards $DIR/categories.json
+python $DIR/manage.py load_flashcards $DIR/temrs.json
+python $DIR/manage.py load_flashcards $DIR/intro.json
+
 # static files
 python $DIR/manage.py collectstatic --noinput
