f7e9e718e8192c806e5744dbb7d89a08897c8cda
Thran <exthran@gmail.com>
2015-04-26 18:34:44 +0200

    update clear data command

--- practice/management/commands/clear_data.py
+++ practice/management/commands/clear_data.py
@@ -1,18 +1,30 @@
 from optparse import make_option
 from django.core.management import BaseCommand
+from proso_flashcards.models import Term, Context, Flashcard
 from proso_models.models import Item
+from practice.models import ExtendedContext
 
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
         make_option(
-            '--clear',
-            dest='clear',
+            '--items',
+            dest='items',
             action='store_true',
             default=False,
-            help='do it'),
+            help='clear all items'),
+        make_option(
+            '--idterms',
+            dest='idterms',
+            action='store_true',
+            default=False,
+            help='clear all terms with int id'),
     )
 
     def handle(self, *args, **options):
-        if options["clear"]:
+        if options["items"]:
             Item.objects.all().delete()
+        if options["idterms"]:
+            for t in Term.objects.all():
+                if t.identifier.isnumeric():
+                    t.delete()
