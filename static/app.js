7e29ce88e9d2605bd76ee1a29c6f294910edf8d9
Thran <exthran@gmail.com>
2015-05-24 16:24:49 +0200

    add level progress and other small improvements

--- static/app.js
+++ static/app.js
@@ -14,7 +14,8 @@ app.factory("global", function(){
 
 app.run(function ($rootScope, $location, userService, global) {
     $rootScope.$on('$routeChangeSuccess', function(){
-        global.progress.active = false;
+        if (global.progress)
+            global.progress.active = false;
         ga('send', 'pageview', $location.path());
     });
 
@@ -92,9 +93,10 @@ app.config(['$routeProvider', '$locationProvider',
         $locationProvider.html5Mode(true);
     }]);
 
-app.controller("panelMenu", function ($scope, global, userService) {
+app.controller("panelMenu", function ($scope, global, userService, areas) {
     $scope.global = global;
     $scope.user = userService;
+    $scope.areas = areas;
 });
 
 app.controller("panelAuth", function ($scope, userService, $location, global) {
@@ -118,10 +120,11 @@ app.controller("auth", function ($scope, userService) {
     };
 });
 
-app.controller("practice", function ($scope, $http, $location, practiceService, global, $routeParams) {
+app.controller("practice", function ($scope, $http, $location, practiceService, global, $routeParams, areas) {
     var area = parseInt($routeParams.id);
     var areaName = $routeParams.areaName;
     var progress = {};
+    areas.setActive(area);
     global.progress = progress;
     $scope.load_flashcards = function(){
         if (area){
@@ -141,6 +144,9 @@ app.controller("practice", function ($scope, $http, $location, practiceService, 
 
     $scope.save_answer = function(answer){
         progress.tries[progress.current] = {correct: answer.correct};
+        if (answer.correct){
+            areas.correctAnswer();
+        }
 
         practiceService.saveAnswerToCurrentFC(
             answer.correct ? $scope.flashcard.id : null,
@@ -252,25 +258,29 @@ app.controller("training", function ($scope, $location, global, areas) {
     };
 
     areas.loadAreas();
+    $scope.max = 10;
+    $scope.dynamic = 7;
 
 });
 
 
 app.factory("areas", function($http, userStatsService){
     var self = this;
-    self.stats = {};
 
     self.loadAreas = function(){
-        $http.get('/flashcards/categorys', {params:{ filter_column: "type", filter_value: "set"}})
+        if (self.areas){
+            self.loadStats();
+            return;
+        }
+        return $http.get('/flashcards/categorys', {params:{ filter_column: "type", filter_value: "set"}})
             .success(function(response){
                 self.areas = response.data;
-                self.loadStats()
+                self.loadStats();
             }
         );
     };
 
     self.loadStats = function(){
-        console.log("sa");
         self.areas.forEach(function(area){
             userStatsService.addGroup(area.id, {categories: [area.id]})
         });
@@ -278,7 +288,44 @@ app.factory("areas", function($http, userStatsService){
             self.areas.forEach(function(area){
                 area.stats = result.data[area.id];
             });
+            self.updateLevels()
         })
+    };
+
+    self.updateLevels = function(){
+        var levelLimits = [0, 3, 10, 20, 50, 100, 200, 500, 1000, 10000];
+        self.areas.forEach(function(area){
+            for (var i=0; i < levelLimits.length; i++) {
+                if (levelLimits[i] <= area.stats.number_of_correct_answers){
+                    area.level = i;
+                    area.nextLevelIn = levelLimits[i + 1] - area.stats.number_of_correct_answers;
+                    area.inThisLevel = area.stats.number_of_correct_answers - levelLimits[i];
+                    area.levelLength = levelLimits[i + 1] -levelLimits[i];
+                }
+            }
+        });
+    };
+
+    self.setActive = function(areaId){
+        if (!self.areas){
+            self.loadAreas().success(function(){
+               self.setActive(areaId);
+            });
+            return;
+        }
+        self.areas.forEach(function(area){
+           if (area.id === areaId){
+               self.active = area;
+           }
+        });
+    };
+
+    self.correctAnswer = function(){
+        if (self.active){
+            self.active.stats.number_of_correct_answers++;
+            self.active.stats.number_of_answers++;
+            self.updateLevels();
+        }
     };
 
     return self;
