379a3cdc24c541d1aa552a9bfe038cf022d7e709
Thran <exthran@gmail.com>
2015-11-14 14:56:19 +0100

    next action on enter press - resolve #2

--- static/js/app.js
+++ static/js/app.js
@@ -172,3 +172,29 @@ app.directive('focusMe', ['$timeout', function($timeout) {
         }
     };
 }]);
+
+app.directive('keypressEvents', ["$document", "$rootScope", function ($document, $rootScope) {
+    return {
+        restrict: 'A',
+        link: function () {
+            $document.bind('keypress', function (e) {
+                $rootScope.$broadcast('keypress', e, String.fromCharCode(e.which));
+            });
+        }
+    };
+}]);
+
+app.directive('nextAction', [function() {
+    return {
+        scope: {
+            condition: '=nextAction'
+        },
+        link: function ($scope, element) {
+            $scope.$on('keypress', function (e, a, key) {
+                if (a.keyCode === 13 && $scope.condition) {
+                    angular.element(element).triggerHandler('click');
+                }
+            });
+        }
+    };
+}]);
