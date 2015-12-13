3d548e8368e125e044e63860e1fc9b430c6d8207
Thran <exthran@gmail.com>
2015-12-13 11:55:51 +0100

    contest - enable caching

--- plantchallenge/settings.py
+++ plantchallenge/settings.py
@@ -190,10 +190,18 @@ LOGGING = {
         }
     }
 }
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+if ON_SERVER:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'LOCATION': '127.0.0.1:11211',
+        }
     }
-}
+else:
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        }
+    }
 
 PROSO_JS_FILES = ['dist/libs.min.js']
